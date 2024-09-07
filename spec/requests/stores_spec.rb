0c5d39538a57de47cc6edb4e0d62680e4626f0b4
Angel D <angeldelacruz1026@gmail.com>
2024-09-07 16:13:48 -0400

    post specs

--- spec/requests/stores_spec.rb
+++ spec/requests/stores_spec.rb
@@ -24,4 +24,28 @@ end
    end
     
   end
+
+  describe 'POST /stores' do
+    # valid payload
+    let(:valid_name) { { name: 'XYZ' } }
+    context 'when the request is valid' do
+      before { post '/stores', params:{'store': valid_name } }
+      it 'creates a store' do
+        expect(json['name']).to eq('XYZ')
+      end
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+    end
+    context 'when the request is invalid' do
+      before { post '/stores', params: {'store': { name: ''} } }
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+    #   it 'returns a validation failure message' do
+    #     expect(response.body)
+    #    .to include("is too short (minimum is 2 characters)")
+    #   end
+    end
+  end
 end
